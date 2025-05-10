160c1adb9ecc65c10554f1e084516eec17510132
zakkwylde313 <zakkwylde313@gmail.com>
2025-05-10 23:40:08 +0900

    fix: remove next-themes for Vite compatibility

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { ThemeProvider } from 'next-themes';
-
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider attribute="class" defaultTheme="light" forcedTheme="light">
-    <App />
-  </ThemeProvider>
+  <App />
 );
